1bbf363d24d138527e7e73e0f0e239b4764994ce
developmentexpert121 <developmentexpert121@gmail.com>
2025-05-02 09:47:03 +0530

    syntax error

--- schemas/chatSchema/appearanceSchema.py
+++ schemas/chatSchema/appearanceSchema.py
@@ -4,24 +4,24 @@ from typing import Optional
 # Pydantic schemas
 class ChatSettingsBase(BaseModel):
     bot_id: int 
-    title_value: str | None
-    title_is_active: bool | None
-    welcome_message_value: str | None
-    welcome_message_is_active: bool | None
-    suggestions_value: str | None
-    suggestions_is_active: bool | None
-    placeholder_value: str | None
-    placeholder_is_active: bool | None
-    # lead_collection: bool | None
-    chat_window_bg: str | None
-    send_button_color: str | None
-    chat_icon: str | None
-    chat_icon_color: str | None
-    user_message_bg: str | None
-    image: str | None
-    dots_color: str | None
-    message_bg: str | None
-    live_message_bg: str | None
+    title_value: Optional[str] = None
+    title_is_active: Optional[bool]= None
+    welcome_message_value: Optional[str] = None
+    welcome_message_is_active: Optional[bool]= None
+    suggestions_value: Optional[str] = None
+    suggestions_is_active: Optional[bool]= None
+    placeholder_value: Optional[str] = None
+    placeholder_is_active: Optional[bool]= None
+    # lead_collection: Optional[bool]= None
+    chat_window_bg: Optional[str] = None
+    send_button_color: Optional[str] = None
+    chat_icon: Optional[str] = None
+    chat_icon_color: Optional[str] = None
+    user_message_bg: Optional[str] = None
+    image: Optional[str] = None
+    dots_color: Optional[str] = None
+    message_bg: Optional[str] = None
+    live_message_bg: Optional[str] = None
     class Config:
         orm_mode = True
 
@@ -34,23 +34,23 @@ class ChatSettingsUpdate(ChatSettingsBase):
 class ChatSettingsRead(ChatSettingsBase):
     id: int
     bot_id:int
-    title_value: str | None
-    title_is_active: bool | None
-    welcome_message_value: str | None
-    welcome_message_is_active: bool | None
-    suggestions_value: str | None
-    suggestions_is_active: bool | None
-    placeholder_value: str | None
-    placeholder_is_active: bool | None
-    lead_collection: bool | None
-    chat_window_bg: str | None
-    send_button_color: str | None
-    chat_icon: str | None
-    chat_icon_color: str | None
-    user_message_bg: str | None
-    image: str | None
-    dots_color: str | None
-    message_bg: str | None
-    live_message_bg: str | None
+    title_value: Optional[str] = None
+    title_is_active: Optional[bool]= None
+    welcome_message_value: Optional[str] = None
+    welcome_message_is_active: Optional[bool]= None
+    suggestions_value: Optional[str] = None
+    suggestions_is_active: Optional[bool]= None
+    placeholder_value: Optional[str] = None
+    placeholder_is_active: Optional[bool]= None
+    lead_collection: Optional[bool]= None
+    chat_window_bg: Optional[str] = None
+    send_button_color: Optional[str] = None
+    chat_icon: Optional[str] = None
+    chat_icon_color: Optional[str] = None
+    user_message_bg: Optional[str] = None
+    image: Optional[str] = None
+    dots_color: Optional[str] = None
+    message_bg: Optional[str] = None
+    live_message_bg: Optional[str] = None
 
 
