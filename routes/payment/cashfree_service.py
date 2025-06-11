7ac044da2bf9f949211a51eab541143e8d8c2859
unknown <developmentexpert121@gmail.com>
2025-06-11 16:48:07 +0530

    payment

--- routes/payment/cashfree_service.py
+++ routes/payment/cashfree_service.py
@@ -297,7 +297,7 @@ async def is_international(request: Request):
     try:
         client_ip = request.client.host
         print(f"Client IP: {client_ip}")
-        country = get_country_from_ip(ip=client_ip)
+        country = await get_country_from_ip(ip=client_ip)
         print("Country", country)
         if country == "IN":
             return {"is_international": False}
